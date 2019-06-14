e4518a295cf8ccb6376cf5a4adee6e214784eb33
blenderfreaky <blendcrafterxiv@gmail.com>
2019-06-14 10:56:14 +0200

    Updated Publisher

---
+++ Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publish("beep");
+            Publish("beepServer");
+        }
+
+        static void Publish(string dir)
+        {
+            foreach (var pathDirectory in Directory.EnumerateDirectories("../../../../" + dir))
+            {
+                var name = Path.GetFileName(pathDirectory);
+                if (name.StartsWith("application"))
+                {
+                    var zipPath = Path.Join(Path.GetDirectoryName(pathDirectory),
+                        "../release",
+                        dir + name.Substring(11));
+                    Directory.CreateDirectory(zipPath);
+                    ZipFile.CreateFromDirectory(pathDirectory, zipPath+".zip");
+                }
+            }
+        }
+    }
+}
